81e0f2ee3fb6058282904e6311bb6dfaae91c25d
AntoineZK <antoine.khalidy@epitech.eu>
2022-09-27 14:21:12 +0000

    [ADD] css to navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import './NavBar.css'
 
 function NavBar() {
     return (
         <nav className="nav">
-            <Link to="/" className="site-title">
-                Site Name
-            </Link>
-        <ul>
-            <Link to="/InternshipReport">Pricing</Link>
-            <br/>
-            <Link to="/InternshipReport/Remerciement">About</Link>
-        </ul>
+            <Link to="/" className="site-title">Site Name</Link>
+            <ul>
+                <li>
+                    <Link to="/InternshipReport" className='section'>Pricing</Link>
+                </li>
+                <li>
+                    <Link to="/InternshipReport/Remerciement" className='section'>About</Link>
+                </li>
+            </ul>
         </nav>
     )
 }
