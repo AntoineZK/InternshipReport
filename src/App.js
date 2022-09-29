64d67f4c60c204522d97023df48967b0b94d7c98
AntoineZK <antoine.khalidy@epitech.eu>
2022-09-29 11:38:54 +0000

    [UPDATE] navbar soon done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React from 'react'
-import NavBar from './components/NavBar/NavBar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import './App.css'
+import React from 'react';
+import './App.css';
+import Navbar from './components/NavBar/NavBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about';
+import Services from './pages/services';
+import Contact from './pages/contact';
+import SignUp from './pages/signup';
 
 function App() {
-    return (
-        <Router>
-            <div>
-                <NavBar />
-                <Routes>
-                    <Route path='/InternshipReport' element={<h1>Test</h1>} />
-                    <Route path='/InternshipReport/Remerciement' element={<h2>Remerciement</h2>} />
-                </Routes>
-            </div>
-        </Router>
-    )
+  	return (
+    	<Router>
+      	<div>
+			<Navbar />
+			<Routes>
+				<Route path='/InternshipReport' element={<Home/>} />
+				<Route path='/InternshipReport/about' element={<About/>} />
+				<Route path='/InternshipReport/services' element={<Services/>} />
+				<Route path='/InternshipReport/contact-us' element={<Contact/>} />
+				<Route path='/InternshipReport/sign-up' element={<SignUp/>} />
+			</Routes>
+		</div>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,34 +1,34 @@
 import React from 'react';
-import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from './Styled.js';
+import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from './Styles';
 
 const sections = [
-    {title: 'Hello', link: '/InternshipReport'},
-    {title: 'World', link: '/InternshipReport/Remerciement'},
+  {title: 'About', link: '/InternshipReport/about'},
+  {title: 'Services', link: '/InternshipReport/services'},
+  {title: 'Contact Us', link: '/InternshipReport/contact-us'},
+  {title: 'Sign Up', link: '/InternshipReport/sign-up'},
 ];
 
-const Navbar = () => {
-  return (
-    <main>
-      <Nav>
-        <NavLink to='/'>
-          <img src={require('../../assets/logo.svg')} alt='logo' />
-        </NavLink>
-        <Bars />
-        <NavMenu>
-            {sections.map((section) => (
-                <NavLink to={section.link} activeStyle>
-                    {section.title}
-                </NavLink>
-            ))}
-          {/* Second Nav */}
-          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
-        </NavMenu>
-        <NavBtn>
-          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
-        </NavBtn>
-      </Nav>
-    </main>
-  );
-};
+function Navbar() {
+	return (
+		<main>
+			<Nav>
+				<NavLink to='/InternshipReport'>
+					<img src={require('../../assets/logo.svg')} alt='logo' />
+				</NavLink>
+				<Bars />
+				<NavMenu>
+					{sections.map((section) => (
+						<NavLink key={section.link} to={section.link}>
+							{section.title}
+						</NavLink>
+					))}
+				</NavMenu>
+				<NavBtn>
+				<NavBtnLink to='/signin'>Sign In</NavBtnLink>
+				</NavBtn>
+			</Nav>
+		</main>
+	)
+}
 
 export default Navbar;
