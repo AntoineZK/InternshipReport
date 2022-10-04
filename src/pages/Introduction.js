f7dd5eb286fa093c132d36b0b3b75de6fbc5266a
AntoineZK <antoine.khalidy@epitech.eu>
2022-10-04 11:29:47 +0000

    [ADD] logo to introduction page

--- src/pages/Introduction.js
+++ src/pages/Introduction.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import PwC_Logo from '../assets/pwc.png'
+import './style.css'
+import { DisplayImage } from './styled';
 
 const Introduction = () => {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '90vh'
-      }}
-    >
-      <h1>Introduction</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1 style={{
+				textAlign: 'center',
+				height: '20vh',
+				color: 'orange',
+				fontWeight: 'bolder'
+			}}>Rapport de stage</h1>
+			<DisplayImage>
+				<img src={PwC_Logo} alt="cur" width={300} height={300}/>
+			</DisplayImage>
+		</div>
+	);
 };
 
 export default Introduction;
