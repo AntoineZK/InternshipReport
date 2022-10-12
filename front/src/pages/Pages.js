6b7dd7101f0d2f6e4f2128011e69fc204e6100fd
AntoineZK_WSL <antoine.khalidy@epitech.eu>
2022-10-12 02:47:21 +0200

    [REVERT] to previous commit

--- front/src/pages/Pages.js
+++
@@ -1,7 +0,0 @@
-import Home from "./Home"
-import Remerciements from "./Remerciements"
-import Plan from "./Plan"
-import Introduction from "./Introduction"
-import Développement from "./Développement"
-import Conclusion from "./Conclusion";
-export { Home, Remerciements, Plan, Introduction, Développement, Conclusion }
