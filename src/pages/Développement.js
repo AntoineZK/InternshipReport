2be879ad9ba9b681f922166ed0b9f5b94946f766
AntoineZK <antoine.khalidy@epitech.eu>
2022-10-19 11:32:24 +0000

    [ADD] start of redacting text for the internshipreport

--- src/pages/Développement.js
+++ src/pages/Développement.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Style.css'
+import PwC_CP_Ext from '../assets/pwcCP.jpg'
+import PwC_CP_Int from '../assets/pwcCP2.jpg'
+import { EnvWork, EnvWork2, Gbl } from './DévText';
 import { NextPages } from '../components/Components';
 
 const TwoLink = [
@@ -16,6 +19,7 @@ const Developpement = ({ closeMobileMenu }) => {
             </div>
             <div id='PwC_Global' className='PwC_Text'>
                 <h2>PwC Global</h2>
+                <p className='paragraph'>{Gbl}</p>
             </div>
             <div id='PwC_Lux' className='PwC_Text'>
                 <h2>PwC Luxembourg</h2>
@@ -28,6 +32,12 @@ const Developpement = ({ closeMobileMenu }) => {
             </div>
             <div id='PwC_Env' className='PwC_Text'>
                 <h2>Environnement de travail</h2>
+                <p className='paragraph'>{EnvWork}</p>
+                <div className='pwcCP'>
+                    <img className='CP_IMG' src={PwC_CP_Ext} alt="PwC_CP_Ext" width={400} height={400}/>
+                    <img className='CP_IMG' src={PwC_CP_Int} alt="PwC_CP_Int" width={400} height={400}/>
+                </div>
+                <p className='paragraph'>{EnvWork2}</p>
             </div>
             <NextPages closeMobileMenu={closeMobileMenu} TwoLink={TwoLink} Status={2}/>
         </div>
