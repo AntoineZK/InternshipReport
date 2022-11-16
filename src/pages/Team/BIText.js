49390ded8312ded1b18466f6d932a2a31425691f
AntoineZK <antoine.khalidy@epitech.eu>
2022-11-16 10:23:48 +0000

    [UPDATE] text

--- src/pages/Team/BIText.js
+++ src/pages/Team/BIText.js
@@ -1,6 +1,23 @@
-const Objectif = ``
+const Objectif = `L'objectif de l'equipe Business Intelligence et la creation, le support, et l'actualistion pour les dashboards et les reports,
 
-const Projet = ``
+`
+
+const Projet = `Lors de mes 3 mois dans l'équipe Business Intelligence j'ai travailler sur different projet :
+
+    - Migration ORACLE vers SQL Server : Le but de ce projet etait comme son nom le dit de migrer l'entierete d'application d'Oracle Database vers SQL Server puisque prochainement PwC
+    n'utilisera plus Oracle Database pour des raisons de coup de license qui augmente vu que de moins en moins de firme national PwC l'augmente et que independamment
+    les coups de la licence Oracle est plus hauts, Oracle Database est un systeme de gestion de base de donnee relationelle autrement dit (SGBDR) qui depuis l'introduction du 
+    modele objet est aussi qualifie de SGBDRO (Système de gestion de base de données relationnel-objet). Pour realiser cette migration il fallait modifier 
+    les types de certaines donnees, verifier si chaque application etait fonctionnelle, que les donnees etait bien remplie avec les nouveaux types de variable,
+    tester si les connecteurs etait bon.
+      
+    - Optimisation Documentum : 
+    
+    - Task / Incident : 
+    
+    - RFC : 
+    
+`
 
 const DiffRen = ``
 
