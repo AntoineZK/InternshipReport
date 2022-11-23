899b1b4b2e0bc385ec1fabfbf62f8ec03712b4ad
AntoineZK <antoine.khalidy@epitech.eu>
2022-11-23 14:58:52 +0000

    [ADD] BI text development

--- src/pages/Team/BIText.js
+++ src/pages/Team/BIText.js
@@ -5,9 +5,9 @@ const Objectif = `L'objectif de l'equipe Business Intelligence et la creation, l
 const Projet = `Lors de mes 3 mois dans l'équipe Business Intelligence j'ai travailler sur different projet :
 
     - Migration ORACLE vers SQL Server : Le but de ce projet etait comme son nom le dit de migrer l'entierete d'application d'Oracle Database vers SQL Server puisque prochainement PwC
-    n'utilisera plus Oracle Database pour des raisons de coup de license qui augmente vu que de moins en moins de firme national PwC l'augmente et que independamment
-    les coups de la licence Oracle est plus hauts, Oracle Database est un systeme de gestion de base de donnee relationelle autrement dit (SGBDR) qui depuis l'introduction du 
-    modele objet est aussi qualifie de SGBDRO (Système de gestion de base de données relationnel-objet). Pour realiser cette migration il fallait modifier 
+    n'utilisera plus Oracle Database pour des raisons de coup de license qui augmente vu que de moins en moins de firme national PriceWaterHouseCoopers l'augmente et que
+    independamment les coups de la licence Oracle est plus hauts, Oracle Database est un systeme de gestion de base de donnee relationelle autrement dit (SGBDR) qui depuis
+    l'introduction du modele objet est aussi qualifie de SGBDRO (Système de gestion de base de données relationnel-objet). Pour realiser cette migration il fallait modifier
     les types de certaines donnees, verifier si chaque application etait fonctionnelle, que les donnees etait bien remplie avec les nouveaux types de variable,
     tester si les connecteurs etait bon.
       
@@ -15,11 +15,24 @@ const Projet = `Lors de mes 3 mois dans l'équipe Business Intelligence j'ai tra
     
     - Task / Incident : 
     
-    - RFC : 
+    - Request For Changes (RFC) : Les RFC
     
 `
 
-const DiffRen = ``
+const DiffRen = `Lors de cette partie de stage je me suis confronte a bien plus de difficulte etant donne que c'etait un domaine dont je n'avais que tres peu de connaissance ni
+d'experience.
+
+Premiere grosse difficulte que j'ai rencontre et la comprehension et l'utilisation des bases de donnees de PwC etant nombreuses et chacune ayant un objectif et une utilisation
+differente, de plus le fait que c'etait la premiere fois que je realise des requetes sql et ma non connaissance des database de PriceWaterHouseCoopers fit que au debut j'etais
+extremement lent pour realiser les taches que j'avais a faire et donc que j'ai eu besoin d'enormement de l'aide de mes collegues pour savoir ou se trouvait telle table ou view
+dans les bases de donnees.
+
+La seconde grosse difficulte est la disponibilite des personnes faisant les requetes de tache ou RFC, etant donne que ces personnes ne sont pas concentrer uniquement sur ce qu'elle
+nous demande il arrive que pendant plusieurs jours j'ai ete bloque par quelque chose qui ne pouvait etre resoudre que grace a l'aide des ces personnes qui soit n'avait pas le
+temps soit etait malade ou en conge
+
+Enfin dernier probleme un petit plus mineur fut les droits d'acces, etant donnee que j'etais stagiaire mon pc etait configure pour avoir des droits d'acces extrement limite ce qui
+fait que sur certaines requete SQL que je realisais il me manquais des droits sur des tables, vue ou meme base de donnee.`
 
 const ConAcq = `Grace aux trois mois realise dans l'equipe business Intelligence, j'ai appris plusieurs chose notamment sur la partie SQL, comme des realisations de query avec 
 des jointures chose que je n'avais que jamais fait, de plus j'ai compris et appris a utiliser Microsoft SQL Server Management Studio (SSMS) qui est un logiciel
@@ -31,9 +44,10 @@ actions comme la creation de Flux qui permet en fonction d'action de transferer 
 voulons fusionners, separer ou vider differentes tables, creer un excel et beaucoup d'autre en placant des sortes de composants qui sont regler pour realiser des actions
 predefini.
 
-Tout au long de mon stage j'ai pu participer a des formations qui m'ont permis de comprendre et de pratiquer sur des applications qui dans le futur pourrait me servir comme
-Power BI 
-
+Tout au long de mon stage j'ai pu participer a des formations qui m'ont permis de comprendre et de pratiquer sur des applications qui dans le futur pourrait me servir comme la
+formation Power BI à laquelle j'ai participe. Power BI est une application developpe par Microsoft pour realise de l'analyse de donnee, cela permet de creer des visualisations de donne personnalise et interactive
+grace a une ineterface simple qui permet aux utilisateurs de realiser leur propres rapport ou Dashboard.
+Power Bi permet de recuperer les donnees de differentes maniere et de differents format, par exemple on peut utiliser les donnes de sources SQL ou Excel pour ne citer qu'eux.
 `
 const Apport = ``
 
